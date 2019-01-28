94275e67483bdb6bc14eb51212e1e3aa3e9f5e45
KjartanOli <kjartanoli@outlook.com>
2019-01-28 08:46:33 +0000

    frumtolur

--- frumtolur.py
+++ frumtolur.py
@@ -1,12 +1,10 @@
 import math
 
-
 for i in range(541, 0, -2):
     isPrime = True
-    for x in range(1, round(math.sqrt(i))):
-        print(i/x)
-        if type(i/x) is int: #Skilar alltaf false.
+    for x in range(2, round(math.sqrt(i))):
+        if i % x == 0:
             isPrime = False
-    #if isPrime:
-        #print(i)
+    if isPrime:
+        print(i)
 print(2)
